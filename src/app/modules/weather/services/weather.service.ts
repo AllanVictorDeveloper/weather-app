4af9040e7092b80133a2c657a5df60876a0ea50d
Allan Santos <developer.allansantos@outlook.com>
2023-10-17 00:06:24 -0300

    first-commit

---
+++ src/app/modules/weather/services/weather.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  private apiKey = '65f2b7e20123f97fe8b338063623c814';
+
+constructor(private http: HttpClient) { }
+
+
+getWeatherDatas(cityName: string): Observable<any>{
+  return this.http
+  .get<any>(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`, {})
+}
+
+
+
+}
